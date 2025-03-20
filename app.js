01081befe99a1f7b4369154b6533287bb4da6ff8
Al-Mehedi Hasan Alif <130339768+HasanAlif@users.noreply.github.com>
2025-03-20 23:32:29 +0600

    Developed Cart Functionality

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 const path = require('path');
+
 const express = require('express');
-
 const csrf = require('csurf');
-
 const expressSession = require('express-session');
 
 const createSessionConfig = require('./config/session');
-
 const db = require('./data/database');
 const addCsrfTokenMiddleware = require('./middlewares/csrf-token');
 const errorHandlerMiddleware = require('./middlewares/error-handler');
@@ -14,10 +12,10 @@ const checkAuthStatusMiddleware = require('./middlewares/check-auth');
 const protectRoutesMiddleware = require('./middlewares/protect-routes');
 const cartMiddleware = require('./middlewares/cart');
 const authRoutes = require('./routes/auth.routes');
-const productRoutes = require('./routes/products.routes');
+const productsRoutes = require('./routes/products.routes');
 const baseRoutes = require('./routes/base.routes');
-const adminRoutes = require('./routes/admin.routes')
-
+const adminRoutes = require('./routes/admin.routes');
+const cartRoutes = require('./routes/cart.routes');
 
 const app = express();
 
@@ -25,29 +23,34 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static('public'));
-app.use('/products/assets',express.static('product-data'));
-app.use(express.urlencoded({extended: false}));
+app.use('/products/assets', express.static('product-data'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 const sessionConfig = createSessionConfig();
 
 app.use(expressSession(sessionConfig));
 app.use(csrf());
 
+app.use(cartMiddleware);
+
 app.use(addCsrfTokenMiddleware);
 app.use(checkAuthStatusMiddleware);
 
 app.use(baseRoutes);
 app.use(authRoutes);
-app.use(productRoutes);
+app.use(productsRoutes);
+app.use('/cart', cartRoutes);
 app.use(protectRoutesMiddleware);
-app.use('/admin',adminRoutes);
+app.use('/admin', adminRoutes);
 
 app.use(errorHandlerMiddleware);
 
-db.connectToDatabase().then(function(){
+db.connectToDatabase()
+  .then(function () {
     app.listen(3000);
-}).catch(function(error){
-    console.log('An error occurred while connecting to the database!');
+  })
+  .catch(function (error) {
+    console.log('Failed to connect to the database!');
     console.log(error);
-});
-
+  });
